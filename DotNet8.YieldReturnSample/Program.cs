bb8a8887e59526671e1189b50d684022fc4a0203
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-23 20:48:25 +0630

    Add project files.

---
+++ DotNet8.YieldReturnSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class PerformanceComparison
+{
+    public static void Main(string[] args)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        foreach (var number in GenerateNumbersWithYield())
+        {
+            // Simulate processing
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken using yield: {stopwatch.ElapsedMilliseconds} ms");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        foreach (var number in GenerateNumbersAllAtOnce())
+        {
+            // Simulate processing
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken returning all at once: {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    public static IEnumerable<int> GenerateNumbersWithYield()
+    {
+        for (int i = 1; i <= 9000000; i++)
+        {
+            yield return i;
+        }
+    }
+
+    public static List<int> GenerateNumbersAllAtOnce()
+    {
+        var numbers = new List<int>();
+        for (int i = 1; i <= 9000000; i++)
+        {
+            numbers.Add(i);
+        }
+
+        return numbers;
+    }
+}
