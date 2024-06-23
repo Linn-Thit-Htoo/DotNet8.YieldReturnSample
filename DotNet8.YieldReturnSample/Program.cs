0cc12b35f0e113c664a6e0c70fab568b2835bbec
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-23 20:49:07 +0630

    -

--- DotNet8.YieldReturnSample/Program.cs
+++ DotNet8.YieldReturnSample/Program.cs
@@ -1,51 +1,50 @@
 ﻿using System.Diagnostics;
 
-namespace DotNet8.YieldReturnSample
+namespace DotNet8.YieldReturnSample;
+
+public class PerformanceComparison
 {
-    public class PerformanceComparison
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        foreach (var number in GenerateNumbersWithYield())
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            foreach (var number in GenerateNumbersWithYield())
-            {
-                // Simulate processing
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine($"Time taken using yield: {stopwatch.ElapsedMilliseconds} ms");
-
-            stopwatch.Reset();
-            stopwatch.Start();
-
-            foreach (var number in GenerateNumbersAllAtOnce())
-            {
-                // Simulate processing
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine($"Time taken returning all at once: {stopwatch.ElapsedMilliseconds} ms");
+            // Simulate processing
         }
 
-        public static IEnumerable<int> GenerateNumbersWithYield()
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken using yield: {stopwatch.ElapsedMilliseconds} ms");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        foreach (var number in GenerateNumbersAllAtOnce())
         {
-            for (int i = 1; i <= 9000000; i++)
-            {
-                yield return i;
-            }
+            // Simulate processing
         }
 
-        public static List<int> GenerateNumbersAllAtOnce()
-        {
-            var numbers = new List<int>();
-            for (int i = 1; i <= 9000000; i++)
-            {
-                numbers.Add(i);
-            }
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken returning all at once: {stopwatch.ElapsedMilliseconds} ms");
+    }
 
-            return numbers;
+    public static IEnumerable<int> GenerateNumbersWithYield()
+    {
+        for (int i = 1; i <= 9000000; i++)
+        {
+            yield return i;
         }
+    }
+
+    public static List<int> GenerateNumbersAllAtOnce()
+    {
+        var numbers = new List<int>();
+        for (int i = 1; i <= 9000000; i++)
+        {
+            numbers.Add(i);
+        }
+
+        return numbers;
     }
 }
